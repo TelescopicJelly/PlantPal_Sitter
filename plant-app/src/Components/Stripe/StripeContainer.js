50dffb95a7459cce9d26206e1131eadeac4e6494
Samari Oglesby <SamariOglesby16@gmail.com>
2025-11-20 17:52:47 -0500

    updates

--- plant-app/src/Components/Stripe/StripeContainer.js
+++ plant-app/src/Components/Stripe/StripeContainer.js
@@ -3,7 +3,7 @@ import {Elements} from '@stripe/react-stripe-js'
 import {loadStripe} from '@stripe/stripe-js'
 import { PaymentForm } from './PaymentForm';
 
-//const PUBLIC_KEY = "pk_test_51QMKqrBSyJvLo4V7zkuIDK8Zw2i7V7AMoRSByj3FDpkgpCKlS726J9p2h6udHekTGuSW8npNeCOU2NtQBvnYiNNT00qwEQXdJH"
+const PUBLIC_KEY = "pk_test_51QMKqrBSyJvLo4V7zkuIDK8Zw2i7V7AMoRSByj3FDpkgpCKlS726J9p2h6udHekTGuSW8npNeCOU2NtQBvnYiNNT00qwEQXdJH"
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
