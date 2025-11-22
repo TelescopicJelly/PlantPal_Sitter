33f952a8839803f9b0c527db4973398c2bad3127
Samari Oglesby <SamariOglesby16@gmail.com>
2025-11-21 21:07:08 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-//import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login } from './Pages/Login/Login';
 import { Register } from './Pages/Register/Register';
-import  Home from './Pages/Home/Home';
+import Home from './Pages/Home/Home';
 import Booking from './Pages/Booking/Booking';
 import Menu from './Pages/Menu/Sitter_Menu';
 import Payment from './Pages/Payment/Payment';
 
-
 function App() {
   return (
- 
-    <Router>
-      <div className="App">
-      {/*<img src={Logo} alt="My Website Logo" className='logo' />*/}
-        
-        <Routes>
-          {/* Default route for "/" renders Login component */}
-          {/*<Route path="/" element={<Navigate to="/login" />} />*/}
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/sitter_menu" element={<Menu />} />
-          <Route path="/payment_form" element={<Payment />} />
-        </Routes>
-      </div>
-    </Router>
+  <Router basename="/PlantPal_Sitter">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/booking" element={<Booking />} />
+      <Route path="/sitter_menu" element={<Menu />} />
+      <Route path="/payment_form" element={<Payment />} />
+    </Routes>
+  </Router>
+
   );
 }
 
