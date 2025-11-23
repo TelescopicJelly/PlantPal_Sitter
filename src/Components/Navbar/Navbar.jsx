dabf96bc9eac1f99dc9cfeff327176288651c0c6
Samari Oglesby <SamariOglesby16@gmail.com>
2025-11-23 02:54:10 -0500

    Updates to page routing

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,30 @@
-import './Navbar.css';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Logo from "../../Copy of Blu’s Greenhouse.png";
-import Logo_name from "../../logo_name.png";
+import './Navbar.css';
+import Logo from '../../Greenhouse.png'; // Make sure image file has no spaces or special chars
+import Logo_name from '../../logoName.png';
 
 const Navbar = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    return (
-        <div className="navbar">
-            <div className="navContainer">
-                <link rel="stylesheet" href="./Navbar.css"></link>
-                <img src={Logo_name} alt="logo" className="logo_name" />
-                <img src={Logo} alt="logo" className="plants_hanging" />
-                <img src={Logo} alt="logo" className="plants_hanging2" />
-                <div className="navItems">
-                    <>
-                        <button className="navButton" onClick={() => navigate('./register')}>Register</button>
-                        <button className="navButton">Login</button>
-                    </>
-                </div>
-            </div>
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <img src={Logo_name} alt="logo" className="logo_name" />
+        <img src={Logo} alt="plants" className="plants_hanging" />
+        <img src={Logo} alt="plants" className="plants_hanging2" />
+        <div className="navItems">
+          <button className="navButton" onClick={() => navigate('/register')}>
+            Register
+          </button>
+          <button className="navButton" onClick={() => navigate('/login')}>
+            Login
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
+
